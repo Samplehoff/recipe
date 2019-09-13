ed968b44a986090d93784283ed69f9dd0f95a20a
Sampey Hoffman <53267153+Samplehoff@users.noreply.github.com>
2019-09-12 21:22:54 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+function marinaraSauce(cookName) {
+   var two = 2;
+   var oneforth = "1/4";
+   
+
+    console.log("Hey," + cookName + " This is my marinara recipe");
+    console.log("Heat a medium-large saucepan over medium heat");
+    console.log("Add " + two + " tspns of oil and 5 garlic cloves, cook until golden, about 2 minutes");
+    console.log("Add " + oneforth + " cup water, " + two + " cans of crushed tomatoes, and salt and season with black pepper to taste");
+    console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+
+}
+
+marinaraSauce("Adam");
